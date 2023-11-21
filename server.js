7ec15ce89a6e2b1c7c9bb9ea9a4a410648a00f22
Pratham5780 <khandelwalg578@gmail.com>
2023-11-21 18:08:17 +0530

    real

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt'); // Import the bcrypt library
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb+srv://khandelwalg578:MZhKWW3twS46SKMX@cluster0.jnvwlce.mongodb.net/?retryWrites=true&w=majority', {
+// Connect to MongoDB (replace 'mongodb://localhost:27017/your-database' with your MongoDB connection URL)
+mongoose.connect('mongodb://localhost:27017/CloudHR', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -17,6 +18,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Define the User schema directly
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
@@ -24,25 +26,32 @@ const userSchema = new mongoose.Schema({
   mobile: String,
 });
 
+// Create the User model
 const User = mongoose.model('User', userSchema);
-
 app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    const passwordMatch = await bcrypt.compare(password, user.password);
-
-    if (passwordMatch) {
-      res.status(200).json({ message: 'Login successful', token });
-    } else {
-      res.status(401).json({ message: 'Invalid credentials' });
+    const { email, password } = req.body;
+  
+    try {
+      const user = await User.findOne({ email });
+  
+      if (!user) {
+        res.status(401).json({ message: 'Invalid credentials' });
+        return;
+      }
+  
+      const passwordMatch = await bcrypt.compare(password, user.password);
+  
+      if (passwordMatch) {
+        res.status(200).json({ message: 'Login successful' });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    } catch (err) {
+      console.error('Error logging in:', err);
+      res.status(500).json({ message: 'Error logging in' });
     }
-  } catch (err) {
-    console.error('Error logging in:', err);
-    res.status(500).json({ message: 'Error logging in' });
-  }
-});
+  });
+// Rest of your code (login route, app.listen, etc.)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
